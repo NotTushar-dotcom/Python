08188b8198c31ad3daaf9ceeff967e3abe0e268f
NotTushar-dotcom <tushh45@gmail.com>
2026-03-03 16:42:06 +0530

    Grade Calculator

---
+++ 02_conditionals/03_solution.py
@@ -0,0 +1,17 @@
+# Grade Calculator
+
+# Assign a letter grade based on a student"s score: A(90-100), B(80-89), C(70-79), D(60-69), F(below 60)
+
+Score =int(input("Enter Total Score:"))
+
+if Score >= 90:
+    print("Grade A")
+elif Score >= 80:
+    print("Grade B")
+elif Score >= 70:
+    print("Grade C")
+elif Score >= 60:
+    print("Grade D")
+else:
+    print("Grade F")
+
